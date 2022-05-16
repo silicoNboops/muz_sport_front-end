358d19ff8f41c6d00d139779790dfcb9913af8a3
Eugene <zhenyaplatonov1@mail.ru>
2022-05-16 17:30:12 +0300

    change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,28 +3,8 @@ import React from "react";
 const Header = () => {
 
     return(
-        // <header className="nks-bg-color">
-        //     <section className="header-main header-height border-bottom">
-        //         <div className="container">
-        //             <div className="row align-items-baseline">
-        //
-        //                 <div className="col-lg-3 col-md-6 p-lg-0 pt-md-2">
-        //                     <a href="/">
-        //                         <img src="assets/images/logo.JPG" width="150px" alt="logo"/>
-        //                     </a>
-        //                 </div>
-        //
-        //                 <div className="col-xl col-lg-1 col-md order-md-1 p-lg-0">
-        //                     <a className="link-nks-light header-link" href="/contacts">Контакты</a>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </section>
-        // </header>
 
-
-    <header
-        className="d-flex flex-wrap align-items-center justify-content-center
+    <header className="d-flex flex-wrap align-items-center justify-content-center
          justify-content-md-between py-2 mb-4 border-bottom">
         <a href="/" className="align-items-center col-md-3 mb-md-0 text-dark text-decoration-none">
             <img src="assets/images/logo.JPG" width="150px" alt="logo"/>
@@ -38,7 +18,18 @@ const Header = () => {
         </ul>
 
         <div className="col-md-3">
-            <button type="button" className="btn btn-primary">Sign-up</button>
+            <div className="icon-link-badged mr-3">
+                <a href="/basket" className="icon-md">
+                    <img src="assets/icons/account.png" width="35px"/>
+                    Вход
+                </a>
+            </div>
+            <div className="icon-link-badged mr-3">
+                <a href="/basket" className="icon-md">
+                    <img src="assets/icons/account.png" width="35px"/>
+                    Регистрация
+                </a>
+            </div>
         </div>
     </header>
     )
