e01ec6a06b2f7f748dc651ece7361d011df84f2a
Egor [zenbook] Zubachenko <zhenyaplatonov1@mail.ru>
2022-05-16 16:16:21 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+
+    return(
+        <header className="container">
+            <section className="padding-y header-main header-height border-bottom">
+                <div className="row align-items-baseline">
+                    <img src="assets/images/logo.JPG" width="150px" alt="logo"/>
+                </div>
+            </section>
+        </header>
+    )
+
+}
+export default Header
