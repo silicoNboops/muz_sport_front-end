33cc6cf3aa4de9c1362dae093d4408177d1b34c6
jsliq_d <egyabig2@gmail.com>
2022-05-21 01:10:27 +0300

    update for OrderPage commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import {BrowserRouter, Route,  Routes} from "react-router-dom";
+import OrderPage from "./pages/OrderPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
               <Header/>
                   <Routes>
                       <Route exact path="/" element={<Home/>} />
+                      <Route exact path="/order" element={<OrderPage/>} />
                   </Routes>
               <Footer/>
           </BrowserRouter>
