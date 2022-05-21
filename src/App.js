755a85752584219edcee9d6f6566bf21e19f2986
jsliq_d <egyabig2@gmail.com>
2022-05-22 02:35:16 +0300

    fix conflicts

--- src/App.js
+++ src/App.js
@@ -1,68 +1,27 @@
-import './assets/css/style.css';
+import './css/style.css';
 import './App.css';
-import Header from "./view/components/Header";
-import React, {useEffect, useState} from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-// import {Footer} from "./pages/Footer";
-import './App.css';
-// import {ToastContainer, Zoom} from "react-toastify";
-// import 'react-toastify/dist/ReactToastify.css';
-// import Authorization from "./components/Authorization";
-import Main from './view/pages/Main';
-import Error404 from './view/pages/Error404';
-import Authorization from "./view/components/auth/Authorization";
-import Account from "./view/components/account/Account";
-import {CartProvider} from 'react-use-cart';
-import {AuthChecker} from "./routing/AuthChecker";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import {BrowserRouter, Route,  Routes} from "react-router-dom";
+import OrderPage from "./pages/OrderPage";
+import OrderPage2 from "./pages/OrderPage2";
+import OrderSuccess from "./pages/OrderSuccess";
 
 function App() {
-    const [modal, setModal] = useState(false);
-
-    const [authToken, setAuthToken] = useState(() =>
-        localStorage.getItem("authToken")
-    );
-
-    const authUser = (token) => {
-        localStorage.setItem("authToken", token);
-        setAuthToken(token);
-    }
-
     return (
-        <CartProvider>
+        <div className="App">
             <BrowserRouter>
-                <div className="App">
-
-                    <Header setModal={setModal} modal={modal} authToken={authToken} setAuthToken={setAuthToken}
-                            authUser={authUser}/>
-
-                    <Routes>
-                        {/*<Route exact path="/" element={<AuthChecker/>}>*/}
-                        {/*    <Route exact path='/' element={<Home/>}/>*/}
-                        {/*</Route>*/}
-                        <Route path="/" element={<Main/>}/>
-                        {/*<Route exact path="/"*/}
-                        {/*       element={<AuthChecker Component={Main} authToken={authToken} />}*/}
-                        {/*/>*/}
-                        {/*<Route path="/error" element={<Error404/>}/>*/}
-                        <Route path="*" element={<Error404/>}/>
-                        <Route path="/login" element={<Authorization authUser={authUser}/>}/>
-                        {/*<Route exact path="/login"*/}
-                        {/*       element={<AuthChecker Component={Authorization} authToken={authToken} />}*/}
-                        {/*/>*/}
-                        <Route exact path="/register"
-                               element={<AuthChecker Component={Authorization} authUser={authUser} />}
-                        />
-                        {/*<Route path="/account"*/}
-                        {/*       element={<AuthChecker Component={Account} authToken={authToken} />}*/}
-                        {/*/>*/}
-                        <Route path="/account" element={<Account/>}/>
-                        <Route exact path="/logout"
-                               element={<AuthChecker Component={Authorization} authUser={authUser} />}
-                        />
-                    </Routes>
-                </div>
+                <Header/>
+                <Routes>
+                    <Route exact path="/" element={<Home/>} />
+                    <Route exact path="/order" element={<OrderPage/>} />
+                    <Route exact path="/order2" element={<OrderPage2/>} />
+                    <Route exact path="/order3" element={<OrderSuccess/>} />
+                </Routes>
+                <Footer/>
             </BrowserRouter>
-        </CartProvider>
+        </div>
     );
 }
 
