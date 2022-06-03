bb7f27e5ceff6241c4a85d8221cbb58b478baee6
jsliq_d <egyabig2@gmail.com>
2022-06-04 01:46:55 +0300

    another try to merge 1

--- src/view/components/Header.js
+++ src/view/components/Header.js
@@ -1,38 +1,97 @@
 import React from "react";
+import {ModalAuth} from "./auth/ModalAuth";
+import API from "../../api/API";
+import { useNavigate } from "react-router-dom";
 
-const Header = () => {
 
-    return(
+const Header = (props) => {
 
-        <header className="d-flex header flex-wrap align-items-center justify-content-center
-         justify-content-md-between py-3 border-bottom">
+    const navigate = useNavigate();
+
+    const logout = (e) => {
+        e.preventDefault();
+
+        API.logout()
+            .then((res) => {
+                // стираем токен
+                localStorage.removeItem("authToken");
+                props.setAuthToken(null);
+
+                // 204 - нет контента в ответе
+                if (res.status === 204) {
+                    navigate('/', { replace: true });
+                }
+            })
+            .catch((error) => {
+                console.log('error-logout');
+            });
+    }
+
+    return (
+        <header className="d-flex flex-wrap align-items-center justify-content-center
+         justify-content-md-between py-2 mb-4 border-bottom">
+
+            {props.modal ?
+                // TODO юзать другой компонент
+                <ModalAuth
+                    authUser={props.authUser}
+                    authToken={props.authToken}
+                    setAuthToken={props.setAuthToken}
+                    setModal={props.setModal}
+                    modal={props.modal}
+                /> : <></>}
+
             <a href="/" className="align-items-center col-md-3 mb-md-0 text-dark text-decoration-none">
-                <img src="assets/images/logo.png" width="140px" alt="logo"/>
+                <img src="assets/images/logo.JPG" width="150px" alt="logo"/>
             </a>
 
             <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" className="nav-link px-2 link-dark">Каталог</a></li>
-                <li><a href="#" className="nav-link px-2 link-dark">Услуги</a></li>
-                <li><a href="#" className="nav-link px-2 link-dark">Партнерство</a></li>
-                <li><a href="#" className="nav-link px-2 link-dark">Проект</a></li>
+                <li><a href="/dodo" className="nav-link px-2 link-secondary">Каталог</a></li>
+                <li><a href="/lol" className="nav-link px-2 link-dark">Услуги</a></li>
+                <li><a href="/aga" className="nav-link px-2 link-dark">Партнерство</a></li>
+                <li><a href="/s" className="nav-link px-2 link-dark">Проект</a></li>
             </ul>
 
             <div className="col-md-3">
-                <div className="icon-link-badged px-2">
-                    <a href="#" className="text-black">
-                        <img src="assets/icons/account.png" width="35px"/>
-                        Вход
-                    </a>
-                </div>
-                <div className="icon-link-badged px-2">
-                    <a href="#" className="text-black">
-                        <img src="assets/icons/account.png" width="35px"/>
-                        Регистрация
-                    </a>
-                </div>
+                {props.authToken === null ? (
+                    <>
+                        <div className="icon-link-badged mr-3">
+                            <a href="/login" className="icon-md">
+                                <img src="assets/icons/account.png" width="35px"/>
+                                Вход
+                            </a>
+                        </div>
+                        <div className="icon-link-badged mr-3">
+                            <a href="/register" className="icon-md">
+                                <img src="assets/icons/account.png" width="35px"/>
+                                Регистрация
+                            </a>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className="icon-link-badged mr-3">
+                            <a href="/account" className="icon-md">
+                                <img src="assets/icons/account.png" width="35px"/>
+                                Личный кабинет
+                            </a>
+                        </div>
+                        <div className="icon-link-badged mr-3">
+                            {/*<a href="/logout" className="icon-md">*/}
+                            {/*    <img src="assets/icons/account.png" width="35px"/>*/}
+                            {/*    Выйти*/}
+                            {/*</a>*/}
+                            <button className="icon-md"
+                                    type="button"
+                                    onClick={logout}>
+                                Выйти
+                            </button>
+                        </div>
+                    </>
+                )}
             </div>
         </header>
     )
 
 }
-export default Header
+export default Header;
