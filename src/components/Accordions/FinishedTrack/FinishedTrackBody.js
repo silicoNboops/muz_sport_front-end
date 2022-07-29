71c74efbbff9585dfd2399bab3684cf6d30bf35e
Eugene <zhenyaplatonov1@mail.ru>
2022-07-29 23:50:46 +0300

    fixed FinishedTrackBody

--- src/components/Accordions/FinishedTrack/FinishedTrackBody.js
+++ src/components/Accordions/FinishedTrack/FinishedTrackBody.js
@@ -121,33 +121,33 @@ const FinishedTrackBody = ({product, price, setPrice}) => {
                     // notifyOrderError();
                 })
     };
-
-    // TODO form to sent order to back
     return (
         <div id={'finished_track' + product.id} className="accordion-collapse collapse pe-5" aria-labelledby="headingOne"
              data-bs-parent="#accordionExample">
-            <form onSubmit={placeOrder}>
-
-                <div className="col-2 imgblock-accordion">
-                    <img src='assets/icons/price-tag.png' className='price-icon-accordion'/>
-                    <span className="text-white">{product.price} ₽</span>
+            <button type="button"
+                    data-bs-toggle="collapse"
+                    className="btn-close shadow-none close-accordion"
+                    data-bs-target={'#finished_track' + product.id}
+                    aria-expanded="true"
+                    style={{borderRadius:"12px"}}
+                    aria-controls="collapseOne">
+            </button>
+            <div className="card background-accordion ps-5 container">
+                <h5 className="text-white position-absolute mt-2" style={{right:"78%"}}>Доработать трек</h5>
+                <div className=" imgblock-accordion">
+                    <img src='/assets/icons/price-tag.png' style={{left: "740%"}} className='price-icon-accordion'/>
+                    <span className="text-black" style={{left:"800px"}}>{price.price_finished_track} ₽</span>
                 </div>
-
-                <div className="accordion-body row justify-content-center">
-                    <div className="card col-8 p-5" style={{borderRadius: "12px"}}>
+                <div className="accordion-body row container justify-content-center mt-4">
+                    <div className="card style-accordion p-5" style={{borderRadius: "12px"}}>
                         <figcaption className="card-props-column">
-
                             <div className="card-props card-text">
                                 <span className="font-weight-bolder">Название трека:</span>
                                 <span className="font-weight-bold container col-6 pb-4">{product.author} - {product.title}</span>
                             </div>
-
                             <div className="card-props card-text">
                                 <span className="font-weight-bolder ">Программа для*:</span>
-
-                                <Form.Group className="col-6 container mb-5" controlId="sports_name">
-                                    {/*TODO либо так, либо span сверху - ПРОТЕСТИТЬ*/}
-                                    {/*<Form.Label className="font-weight-bolder">Программа для:</Form.Label>*/}
+                                <Form.Group className="col-6  container mb-5 ">
                                     <Form.Control
                                         className="text-center"
                                         style={{backgroundColor: "rgba(153,147,196,0.73)", borderRadius:"16px"}}
@@ -156,7 +156,7 @@ const FinishedTrackBody = ({product, price, setPrice}) => {
                                         value={sport}
                                         onChange={(e) => setSport(e.target.value)}
                                     >
-                                        <option>
+                                        <option className="">
                                             Вид спорта
                                         </option>
 
@@ -168,7 +168,6 @@ const FinishedTrackBody = ({product, price, setPrice}) => {
                                     </Form.Control>
                                 </Form.Group>
                             </div>
-
                             <div className="card-props card-text">
                                     <span className="font-weight-bolder">
                                         <img src="assets/icons/info.png" className="mb-1 me-1" width="12px"/>
@@ -194,54 +193,51 @@ const FinishedTrackBody = ({product, price, setPrice}) => {
                                                setTimeEnd(e.target.value)}/>
                                     </span>
                             </div>
-
                             <div className="card-props card-text">
                                 <span className="font-weight-bolder">Пик в начале:</span>
                                 <span className="font-weight-bold">
-                                        <div className="form-check">
-                                          <input className="form-check-input checkbox-accordion"
-                                                 type="checkbox"
-                                                 id="flexCheckDefault"
-                                                 onChange={() => setBeginningPeak(!beginningPeak)}
-                                          />
-                                          <label className="form-check-label"
-                                                 htmlFor="flexCheckDefault">
-                                              Добавить
-                                          </label>
-                                        </div>
-                                    </span>
+                                <div className="position-relative" style={{right: "200%"}}>
+                                  <input type="checkbox" className="checkbox-ex" id="label1"
+                                         onChange={() => setBeginningPeak(!beginningPeak)}
+                                  />
+                                  <label id="label1" htmlFor="label1" className="checkbox-ex">Добавить</label>
+                                </div>
+                            </span>
                             </div>
 
                             <div className="card-props card-text">
                                 <span className="font-weight-bolder">Окончание*:</span>
                                 <span className="font-weight-bold col-8 ps-4">
-                                        <div className="buttons d-grid">
-                                                <input label="Плавное" type="radio" name="end" value="Плавное"
-                                                       checked={end === 'Плавное'}
-                                                       onChange={(e) => {
-                                                           setEnd(e.target.value)
-                                                       }}
-                                                />
-                                                <input label="Резкое" type="radio" name="end" value="Резкое"
-                                                       checked={end === 'Резкое'}
-                                                       onChange={(e) => {
-                                                           setEnd(e.target.value)
-                                                       }}
-                                                />
-                                        </div>
-                                    </span>
+                                    <div className="buttons d-grid">
+                                            <input label="Плавное"
+                                                   type="radio"
+                                                   name="end"
+                                                   value="smooth"
+                                                   checked={end === 'Плавное'}
+                                                   onChange={(e) => {
+                                                       setEnd(e.target.value)
+                                                   }}/>
+                                            <input label="Резкое"
+                                                   type="radio"
+                                                   name="end"
+                                                   value="sharp"
+                                                   checked={end === 'Резкое'}
+                                                   onChange={(e) => {
+                                                       setEnd(e.target.value)
+                                                   }}/>
+                                    </div>
+                                </span>
                             </div>
-
                             <h5 className="text-start">
-                                <img src="assets/icons/info.png" className="mb-1 me-1" width="12px"/>
-                                Компоновка*:</h5>
+                                <img src="/assets/icons/info.png" className="mb-1 me-1" width="12px"/>
+                                Компоновка*</h5>
 
                             <div className="float-start">
-                                <button className="btn btn-sm" onClick={handleAuto}>
-                                    <img src="assets/icons/auto.png" className="float-start" height="50px"/>
+                                <button className="btn btn-sm shadow-none" onClick={handleAuto}>
+                                    <img src="/assets/icons/auto.png" className="float-start" height="50px"/>
                                 </button>
-                                <button className="btn btn-sm" onClick={handleManual}>
-                                    <img src="assets/icons/palm-of-hand.png" className="float-start" height="50px"/>
+                                <button className="btn btn-sm shadow-none" onClick={handleManual}>
+                                    <img src="/assets/icons/palm-of-hand.png" className="float-start" height="50px"/>
                                 </button>
                             </div>
 
@@ -250,25 +246,27 @@ const FinishedTrackBody = ({product, price, setPrice}) => {
                                 :
                                 null
                             }
-
                             {compositionType === 'manual' ?
-                                <div className="float-start">
+                                <div className="col-12 float-start">
                                     <div className="row">
+
                                         <div className="buttons d-grid pt-4">
-                                            <input label="Удалить отрезок" type="radio" name="segment-1" value="delete"
-                                                   checked={compositionSegment1 === 'delete'}
+                                            <input label="Удалить отрезок" type="radio" name="segment1"
+                                                   value="delete_segment" checked={compositionSegment1 === 'delete'}
                                                    onChange={(e) => {
                                                        setCompositionSegment1(e.target.value)
                                                    }}
                                             />
-                                            <input label="Добавить отрезок" type="radio" name="segment-1" value="add"
-                                                   checked={compositionSegment1 === 'add'}
+                                            <input label="Добавить отрезок" type="radio" name="segment1"
+                                                   value="add_segment" checked={compositionSegment1 === 'add'}
                                                    onChange={(e) => {
                                                        setCompositionSegment1(e.target.value)
                                                    }}
                                             />
                                         </div>
-                                        <span className="font-weight-bold col-6 pt-5">от
+
+                                        {DeleteSegment ?
+                                            <span className="font-weight-bold ms-3 text-start d-block col pt-5">от
                                             <input type="time" id="appt" name="appt" style={{
                                                 backgroundColor: "#948eba",
                                                 borderRadius: "10px"
@@ -279,51 +277,35 @@ const FinishedTrackBody = ({product, price, setPrice}) => {
                                                 borderRadius: "10px"
                                             }} min="00:00" max="24:00" className="me-2 ms-2 text-white"
                                                    required/>
-                                            <button type="button" className="btn mb-2 ">
-                                                <img src="assets/icons/plus-purple.png" height="25px"/>
+                                            <button type="button" className="btn mb-2" >
+                                                <img src="/assets/icons/plus-purple.png" height="25px"/>
                                             </button>
-                                            </span>
-
-                                    </div>
-                                    <div className="row col-12">
-                                        <div className="buttons d-grid pt-4">
-                                            <input label="Удалить отрезок" type="radio" name="segment-2" value="delete"
-                                                   checked={compositionSegment2 === 'delete'}
-                                                   onChange={(e) => {
-                                                       setCompositionSegment2(e.target.value)
-                                                   }}
-                                            />
-                                            <input label="Добавить отрезок" type="radio" name="segment-2" value="add"
-                                                   checked={compositionSegment2 === 'add'}
-                                                   onChange={(e) => {
-                                                       setCompositionSegment2(e.target.value)
-                                                   }}
-                                            />
-                                        </div>
-                                        <span className="font-weight-bold col-7 pt-5">от
+                                        </span>
+                                            :
+                                            <span className="font-weight-bold d-block col pt-5">от
                                             <input type="time" id="appt" name="appt" style={{
                                                 backgroundColor: "#948eba",
                                                 borderRadius: "10px"
                                             }} min="00:00" max="24:00" className="me-2 ms-2 text-white"
                                                    required/>до
-                                            <input type="time" id="appt" name="appt" style={{
-                                                backgroundColor: "#948eba",
-                                                borderRadius: "10px"
-                                            }} min="00:00" max="24:00" className="me-2 ms-2 text-white"
-                                                   required/>на
+                                             <input type="time" id="appt" name="appt" style={{
+                                                 backgroundColor: "#948eba",
+                                                 borderRadius: "10px"
+                                             }} min="00:00" max="24:00" className="me-2 ms-2 text-white"
+                                                    required/>на
                                             <input type="time" id="appt" name="appt" style={{
                                                 backgroundColor: "#948eba",
                                                 borderRadius: "10px"
                                             }} min="00:00" max="24:00" className="me-2 ms-2 text-white"
                                                    required/>
-
-                                            <button type="button" className="btn mb-2">
-                                                <img src="assets/icons/plus-purple.png" height="25px"/>
+                                            <button type="button" className="btn mb-2" onClick={addCount}>
+                                                <img src="/assets/icons/plus-purple.png" height="25px"/>
                                             </button>
-                                            <button type="button" className="btn mb-2">
-                                                <img src="assets/icons/minus-pink.png" height="25px"/>
+                                            <button type="button" className="btn mb-2" onClick={deleteCount}>
+                                                <img src="/assets/icons/minus-pink.png" height="25px"/>
                                             </button>
-                                            </span>
+                                        </span>
+                                        }
                                     </div>
                                 </div>
                                 :
@@ -340,29 +322,31 @@ const FinishedTrackBody = ({product, price, setPrice}) => {
                                       className="form-control input-box"
                                       style={{minHeight: "150px", backgroundColor: "#f4f4f4"}}
                                       placeholder="Пример:
-                                       -Оставить все энергичные части трека
-                                       или
-                                       -Убрать все части с вокалом"
+                                   -Оставить все энергичные части трека
+                                   или
+                                   -Убрать все части с вокалом"
                                       onChange={(e) =>
                                           setCommentary(e.target.value)}/>
                         </div>
                     </div>
 
-                    <AdditionalTrackBtn product={product} price={price}/>
-                    <AdditionalTrackBody product={product} price={price}
-                                         setAdditionalTracks={setAdditionalTracks} />
 
-                    <AddSuggestiveEffectBtn product={product} price={price}/>
-                    <AddSuggestiveEffectBody product={product} price={price}
-                                             setSuggestiveEffect={setSuggestiveEffect}/>
-
-                    <UnloadingModuleBtn product={product} price={price}/>
-                    <UnloadingModuleBody product={product} price={price}
-                                            setUnloadingModule={setUnloadingModule}/>
-
-                    {/*<SpecialWishes price={price}/>*/}
                 </div>
-            </form>
+            </div>
+
+            <AdditionalTrackBtn product={product} price={price}/>
+            <AdditionalTrackBody product={product} price={price}
+                                 setAdditionalTracks={setAdditionalTracks} />
+
+            <AddSuggestiveEffectBtn product={product} price={price}/>
+            <AddSuggestiveEffectBody product={product} price={price}
+                                     setSuggestiveEffect={setSuggestiveEffect}/>
+
+            <UnloadingModuleBtn product={product} price={price}/>
+            <UnloadingModuleBody product={product} price={price}
+                                 setUnloadingModule={setUnloadingModule}/>
+
+            {/*<SpecialWishes price={price}/>*/}
         </div>
     )
 }
